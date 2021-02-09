feeb0f9c778cffdc428c3de7989d93a1f32f6bda
Callen Johnston-Adams <2467512j@student.gla.ac.uk>
2021-02-09 20:17:37 +0000

    Finished chapter 9 exercises, add_category & add_page pages are now restricted, code does does not pass tests_chapter9. Trying to fix.

--- rango/views.py
+++ rango/views.py
@@ -66,6 +66,7 @@ def show_category(request, category_name_slug):
 	return render(request, 'rango/category.html', context=context_dict)
 
 
+@login_required
 def add_category(request):
 	form = CategoryForm()
 
@@ -90,6 +91,7 @@ def add_category(request):
 	return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, category_name_slug):
 	try:
 		category = Category.objects.get(slug=category_name_slug)
@@ -221,10 +223,9 @@ def user_login(request):
 		return render(request, 'rango/login.html')
 
 
+@login_required
 def restricted(request):
-	@login_required
-	def restricted(request):
-		return HttpResponse("Since you're logged in, you can see this text!")
+	return redirect(reverse("rango:restricted"))
 
 
 @login_required
