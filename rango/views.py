7c37fe721f8c7ce3b2e37e7efb2ce5dfe02716be
Callen Johnston-Adams <2467512j@student.gla.ac.uk>
2021-02-11 19:58:10 +0000

    Added cookies to count number of visits and time of last visit to the index page

--- rango/views.py
+++ rango/views.py
@@ -5,16 +5,36 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 # Create your views here.
 
+# 2 helper functions for chapter 10
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		# Update the last visit cookie now that we have updated the count
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		# Set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+
+	# Update/set the visits cookie
+	request.session['visits'] = visits
+
 
 def index(request):
-	# Query the database for a list of ALL categories currently stored.
-	# Order the categories by the number of likes in descending order.
-	# Retrieve the top 5 only -- or all if less than 5.
-	# Place the list in our context_dict dictionary (with our boldmessage!)
-	# that will be passed to the template engine.
 	category_list = Category.objects.order_by('-likes')[:5]
 	pages_list = Page.objects.order_by('-views')[:5]
 
@@ -23,8 +43,16 @@ def index(request):
 	context_dict['categories'] = category_list
 	context_dict['pages'] = pages_list
 
+
+	# Call the helper function to handle the cookies
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
+	# Obtain our Response object early so we can add cookie information
+	response = render(request, 'rango/index.html', context=context_dict)
+	return response
 	# Render the response and send it back!
-	return render(request, 'rango/index.html', context=context_dict)
+	# return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
@@ -225,7 +253,8 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-	return redirect(reverse("rango:restricted"))
+	return render(request, "rango/restricted.html")
+	# return redirect(reverse("rango:restricted")) # instructed to make this edit by vishrut (wad lab person)
 
 
 @login_required
